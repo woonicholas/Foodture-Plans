3f80ac0583169146a6d54c363f449d4d3d16a75b
Anton Yeung <admin@Antons-MacBook-Air.local>
2021-01-25 22:38:00 -0800

    login and register UI

--- mobile-ui/src/app/login-modal/login-modal.component.ts
+++ mobile-ui/src/app/login-modal/login-modal.component.ts
@@ -1,14 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { ModalController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login-modal',
   templateUrl: './login-modal.component.html',
   styleUrls: ['./login-modal.component.scss'],
 })
-export class LoginModalComponent implements OnInit {
+export class LoginModalComponent {
 
-  constructor() { }
+  constructor(private modalCtrl:ModalController) { }
 
-  ngOnInit() {}
+  dismissModal() {
+    this.modalCtrl.dismiss();
+  }
+
+  onLoginSubmit(){
+    console.log("Login Success")
+  }
 
 }
