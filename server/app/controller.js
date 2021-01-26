0a2cea313c2eb085843eb7c98fccd6b88c9eca48
woonicholas <nickj_woo@yahoo.com>
2021-01-25 18:40:58 -0800

    mobile ui initial commit

--- server/app/controller.js
+++ server/app/controller.js
@@ -25,7 +25,7 @@ exports.signup = (req, res) => {
     .catch((error) => {
         var errorCode = error.code;
         var errorMessage = error.message;
-        res.status(400).send(error)
+        res.status(400).send(error);
     });
         
 }
